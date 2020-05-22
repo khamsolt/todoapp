5c8f397c2bf2551c2cfeffe146b76b5da17e8d05
khamsolt <khamidov.che@gmail.com>
2020-05-22 17:36:44 +0300

    0.0.2 Notes model and migrations have been added.

---
+++ database/migrations/2020_05_22_135852_create_notes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onDelete('CASCADE')
+                ->onUpdate('CASCADE');
+            $table->text('content')
+                ->nullable();
+            $table->enum('status', ['opened', 'closed'])
+                ->default('opened');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
